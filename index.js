55d2778a076dd284aae6432d75c68375845579d1
swanITworksl <michal@labi.pl>
2019-12-05 17:05:33 +0100

    base Sass configuration

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-import React, {Component} from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import './scss/main.scss';
 import App from "./app";
-
-
 
 ReactDOM.render(<App/>, document.getElementById("app"));
 
