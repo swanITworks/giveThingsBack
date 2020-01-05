ec4a2efbef9b48002fe903945375fc27e54e5b41
swanITworksl <michal@labi.pl>
2020-01-05 12:14:26 +0100

    v2.1

--- components/Home/HomeContactUs.js
+++ components/Home/HomeContactUs.js
@@ -6,18 +6,43 @@ const contactTemplate = {
     message: '',
 };
 
+const warningsTemplate = {
+    name: '',
+    email: '',
+    message: '',
+};
+
 function HomeContactUs() {
 
     const [contactInputs, setContactInputs] = useState(contactTemplate);
-    const [warnings, setWarnings] = useState('');
+    const [warnings, setWarnings] = useState(warningsTemplate);
 
-    const handlerInputsContact = (e) => {
+    const handlerInputsContactChange = (e) => {
         const {name, value} = e.target;
         setContactInputs(prevState => ({...prevState, [name]: value}));
     };
 
+
     const handlerSubmitContact = (e) => {
-        e.target.preventDefault();
+        e.preventDefault();
+        if (contactInputs.name.length < 2) {
+            setWarnings(prevState => ({...prevState, name: <div>name should have minimum 2 characters</div>}))
+        } else {
+            setWarnings(prevState => ({...prevState, name:''}))
+        }
+
+        if (contactInputs.email.length < 2) {
+            setWarnings(prevState => ({...prevState, email: 'email should have minimum 2 characters'}))
+        } else {
+            setWarnings(prevState => ({...prevState, email:''}))
+        }
+
+        if (contactInputs.message.length < 2) {
+            setWarnings(prevState => ({...prevState, message: 'message should have minimum 2 characters'}))
+        } else {
+            setWarnings(prevState => ({...prevState, message:''}))
+        }
+
     };
 
     return (
@@ -28,16 +53,17 @@ function HomeContactUs() {
                 <form onSubmit={handlerSubmitContact}>
                     <div className='topOfForm'>
                         <label>Write your name<input name='name' type='name' placeholder='name'
-                                                     onChange={handlerInputsContact} value={contactInputs.name}/></label>
+                                                     onChange={handlerInputsContactChange}
+                                                     value={contactInputs.name}/>{warnings.name}</label>
                         <label>Write your email<input name='email' type='email' placeholder='email'
-                                                      onChange={handlerInputsContact}
-                                                      value={contactInputs.email}/></label>
+                                                      onChange={handlerInputsContactChange}
+                                                      value={contactInputs.email}/>{warnings.email}</label>
                     </div>
                     <div className='mainForm'>
-                        <label>Write your message<br/><textarea name='message' rows='5' onChange={handlerInputsContact}
+                        <label>Write your message<br/><textarea name='message' rows='5' onChange={handlerInputsContactChange}
                                                                 value={contactInputs.message}
                                                                 placeholder='Lorem ipsum dolor sit amet, consectetur
-                                                                adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'/></label>
+                                                                adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'/>{warnings.message}</label>
                     </div>
                     <button type='submit' className='sendButton'>Send
                     </button>
