7cd54fb82bbc8f15cdb31eab6b11b84766fbf688
swanITworksl <michal@labi.pl>
2020-01-08 20:37:42 +0100

    v2.3

--- components/LogSing/SingIn.js
+++ components/LogSing/SingIn.js
@@ -1,19 +1,22 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import HomeHeaderMenu from "../Home/HomeHeaderMenu";
 import {NavLink} from "react-router-dom";
 import firebase from "../../config";
 
 const signInInputsTemplate = {
     email: '',
-    warningLogin: '',
+    warningLogin: ' ',
     password: '',
-    warningPassword: '',
+    warningPassword: ' ',
     passwordRepeat: '',
-    warningPasswordRepeat: '',
+    warningPasswordRepeat: ' ',
     validationChanger: false,
+    success: '',
 };
 
-function SignIn(props) {
+const db = firebase.firestore();
+
+function SingIn(props) {
     const [signInInputs, setSignInInputs] = useState(signInInputsTemplate);
     const isLogIn = props.isLogIn;
 
@@ -22,58 +25,123 @@ function SignIn(props) {
         setSignInInputs({...signInInputs, [name]: value});
     };
 
+    useEffect(() => {
+        if (signInInputs.warningLogin === '' && signInInputs.warningPassword === '' && signInInputs.warningPasswordRepeat === '') {
+            sendData();
+        }
+    }, [signInInputs.validationChanger]);
+
     const handlerSignIn = (e) => {
         e.preventDefault();
-        if (signInInputs.email.length <= 2) {
+        if (signInInputs.email.length <= 4 || (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(signInInputs.email) === false)) {
             setSignInInputs(prevState => ({
                 ...prevState,
-                warningLogin: <div>Login is to short, should have minimum 2 characters</div>,
+                warningLogin: <div className='signInWarnings'>Email is to short, should have minimum 4 characters and
+                    include '@' and '.'</div>,
+                validationChanger: !prevState.validationChanger,
+            }))
+        } else {
+            setSignInInputs(prevState => ({
+                ...prevState,
+                warningLogin: '',
+                validationChanger: !prevState.validationChanger,
             }))
         }
-
-
+        if (signInInputs.password.length < 6) {
+            setSignInInputs(prevState => ({
+                ...prevState,
+                warningPassword: <div className='signInWarnings'>Password is to short, should have minimum 6
+                    characters</div>,
+                validationChanger: !prevState.validationChanger,
+            }))
+        } else {
+            setSignInInputs(prevState => ({
+                ...prevState,
+                warningPassword: '',
+                validationChanger: !prevState.validationChanger,
+            }))
+        }
+        if (signInInputs.password !== signInInputs.passwordRepeat) {
+            setSignInInputs(prevState => ({
+                ...prevState,
+                warningPasswordRepeat: <div className='signInWarnings'>Repetition should be the same like
+                    password</div>,
+                validationChanger: !prevState.validationChanger,
+            }))
+        } else {
+            setSignInInputs(prevState => ({
+                ...prevState,
+                warningPasswordRepeat: '',
+                validationChanger: !prevState.validationChanger,
+            }))
+        }
     };
 
     const sendData = () => {
-        const db = firebase.firestore();
         db.collection('users')
-            .doc(signInInputs.login)
+            .doc(signInInputs.email)
             .set({
                 login: signInInputs.email,
                 password: signInInputs.password,
-            }).then(function () {
+            })
+            .then(function () {
             console.log("Document successfully written!");
+            setSignInInputs(prevState => (
+                    {
+                        ...prevState,
+                        email:'',
+                        password:'',
+                        passwordRepeat:'',
+                        success: <div className='success'>Your account was created. Please log in</div>
+                    }
+                )
+            )
         })
             .catch(function (error) {
                 console.error("Error writing document: ", error);
             });
     };
 
+    const checkUserIfExist = () => {
+        db.collection('users')
+            .where('login', '==', logInData.login)
+            .where('password', '==', logInData.password)
+            .get()
+            .then(function (querySnapshot) {
+                if (querySnapshot.docs.length !== 0) {
+                    logInHandler();
+                    console.log('jest user');
+                }
+            })
+    };
+
     return (
-        <div className='logIn'>
+        <div className='signIn'>
             <HomeHeaderMenu isLogIn={isLogIn}/>
-            <div className='logInHeader'>
+            <div className='signInHeader'>
                 <h1>Sign In</h1>
                 <img src='../../assets/Decoration.svg'/>
+                {signInInputs.success !== '' ? signInInputs.success : null}
             </div>
             <form onSubmit={handlerSignIn}>
-                <div className='logInBox'>
+                <div className='signInBox'>
                     <label>Email</label><input type='text' name='email' value={signInInputs.email}
                                                onChange={handlerSignInInputsChange}/>{signInInputs.warningLogin}
                     <label>Password</label><input type='password' name='password' value={signInInputs.password}
-                                                  onChange={handlerSignInInputsChange}/>
+                                                  onChange={handlerSignInInputsChange}/>{signInInputs.warningPassword}
                     <label>Repeat Password</label><input type='password' name='passwordRepeat'
                                                          value={signInInputs.passwordRepeat}
-                                                         onChange={handlerSignInInputsChange}/>
+                                                         onChange={handlerSignInInputsChange}/>{signInInputs.warningPasswordRepeat}
                 </div>
-                <div className='logInButtons'>
+                <div className='signInButtons'>
                     <NavLink exact to="/logIn" activeClassName="active" className='button'>Log in</NavLink>
                     <button type='submit' activeClassName="active" className='button'>Sign
-                        in</button>
+                        in
+                    </button>
                 </div>
             </form>
         </div>
     )
 }
 
-export default SignIn;
+export default SingIn;
